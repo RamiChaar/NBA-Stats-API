b81bc7ee9ebddd5d3273dae0d27dd6278b2f89ad
RamiChaar <Chaarrami22@gmail.com>
2022-12-31 13:51:13 -0800

    added year query parameter
    year parameter now allows users to query a players specific season averages by year

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -23,11 +24,21 @@ const SeasonAvg = require('./seasonAvg');
 app.get('/api/player', (req, res) => {
 
     const queryId = req.query.id;
-
+    const year = parseInt(req.query.year);
+    console.log(year)
+    
     if(queryId === undefined){
         res.status(200).send({
             error: "no query id",
             response: "must provide query id in url, composed of first 5 letters of last name followed by first 2 letters of first name. ex: Michael Jordan -> ?id=jordami"
+        })
+        return;
+    }
+
+    if( req.query.year !== undefined && ( isNaN(year) || (1949 > year || year > 2021) )){
+        res.status(200).send({
+            error: `${req.query.year} is an invalid year`,
+            response: "year must be a number between 1949 and 2021 inclusive. ex: Larry Bird's 1981-1982 season -> ?id=birdla&year=1981"
         })
         return;
     }
@@ -51,8 +62,41 @@ app.get('/api/player', (req, res) => {
             
             if(queriedPlayers.length === 1) {
 
-                const seasonAverages = await SeasonAvg.find({ PlayerAdditional : queriedPlayers[0].PlayerAdditional}).sort({Age : 1});
+                let playerObj = {
+                    PlayerId : queriedPlayers[0].PlayerAdditional,
+                    name : queriedPlayers[0].Player,
+                    BirthDate : queriedPlayers[0].BirthDate,
+                    position : queriedPlayers[0].Pos,
+                    playedFrom : queriedPlayers[0].From,
+                    playedTo : queriedPlayers[0].To,
+                    Weight : queriedPlayers[0].Weight,
+                    height : {
+                        feet : queriedPlayers[0].Feet,
+                        inches : queriedPlayers[0].Inches
+                    },
+                    Colleges : queriedPlayers[0].Colleges,
+                }
 
+                let seasonAverages = [];
+
+                if(req.query.year !== undefined){
+
+                    seasonAverages = await SeasonAvg.find({ PlayerAdditional : queriedPlayers[0].PlayerAdditional, Year : new RegExp( year + '-.*')});
+
+                    if(seasonAverages.length === 0){
+                        res.status(200).send({
+                            error: `${playerObj.name} has no record during the ${year}-${(year+1)} season`,
+                            response: `${playerObj.name} played between the seasons of ${queriedPlayers[0].From-1}-${queriedPlayers[0].From} and ${queriedPlayers[0].To-1}-${queriedPlayers[0].To}`,
+                        })
+                        return;
+                    }
+
+                } else {
+
+                    seasonAverages = await SeasonAvg.find({ PlayerAdditional : queriedPlayers[0].PlayerAdditional}).sort({Age : 1});
+
+                }
+                    
                 let seasonsArr = [];
                 seasonAverages.forEach((season) => {
                     let seasonObj = {
@@ -92,25 +136,16 @@ app.get('/api/player', (req, res) => {
                         avgTOV : season.TOV,
                         avgPF : season.PF,
                     };
-
                     seasonsArr.push(seasonObj);
                 })
+                
+                if(req.query.year !== undefined){
+                    playerObj[year + '-' + (year+1)] = seasonsArr;
+                } else {
+                    playerObj["seasonAverages"] = seasonsArr;
+                }
 
-                res.status(200).send({
-                    name : queriedPlayers[0].Player,
-                    BirthDate : queriedPlayers[0].BirthDate,
-                    position : queriedPlayers[0].Pos,
-                    playedFrom : queriedPlayers[0].From,
-                    playedTo : queriedPlayers[0].To,
-                    Weight : queriedPlayers[0].Weight,
-                    height : {
-                        feet : queriedPlayers[0].Feet,
-                        inches : queriedPlayers[0].Inches
-                    },
-                    Colleges : queriedPlayers[0].Colleges,
-                    PlayerId : queriedPlayers[0].PlayerAdditional,
-                    seasonAverages : seasonsArr
-                })
+                res.status(200).send(playerObj)
 
                 return;
 
@@ -120,10 +155,10 @@ app.get('/api/player', (req, res) => {
                 let playerArr = [];
                 queriedPlayers.forEach((player) => {
                     playerObj = {
+                        PlayerId : player.PlayerAdditional,
                         name : player.Player,
                         playedFrom : player.From,
                         playedTo : player.To,
-                        PlayerId : player.PlayerAdditional
                     }
                     playerArr.push(playerObj)
                 })
